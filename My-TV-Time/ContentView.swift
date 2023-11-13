7fd74706755391eb896e330af88f8d038fe172b6
Mohammad Solki <53335722+msolki@users.noreply.github.com>
2023-11-13 15:39:59 +0100

    Initial Commit

---
+++ My-TV-Time/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  My-TV-Time
+//
+//  Created by Mohammad Solki on 13/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ My-TV-Time/My_TV_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_TV_TimeApp.swift
+//  My-TV-Time
+//
+//  Created by Mohammad Solki on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct My_TV_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
